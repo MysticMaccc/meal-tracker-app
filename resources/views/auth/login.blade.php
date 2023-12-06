abdb56237b7c6a712dc1310577e20ff3271ecf8b
MysticMaccc <cosmicsher96@gmail.com>
2023-12-06 13:29:52 +0800

    Editing landing page and login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,68 @@
 <x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+    
+    <main>
+        <section class="container d-flex flex-column vh-100">
+            <div class="row align-items-center justify-content-center g-0 h-lg-100 py-8">
+                <div class="col-lg-5 col-md-8 py-8 py-xl-0">
+                    <!-- Card -->
+                    <div class="card shadow">
+                        <!-- Card body -->
+                        <div class="card-body p-6">
+                            <div class="mb-4">
+                                <a href="../index.html"><img src="{{ asset('assets/images/meal-tracking-images/logo.png') }}" class="img-fluid rounded-3" alt="logo-icon"></a>
+                                <h1 class="mb-1 fw-bold">Sign in</h1>
+                                <span>
+                                    Don’t have an account?
+                                    <a href="{{ route('login') }}" class="ms-1" wire:navigate>Sign up</a>
+                                </span>
+                            </div>
+                            
+                            <x-submit-message class="mb-4" />
 
-        <x-validation-errors class="mb-4" />
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username">
+                                </div>
+                                
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Password</label>
+                                    <input id="password" class="form-control" type="password" name="password" required autocomplete="current-password" >
+                                </div>
+                                
+                                <!-- Checkbox -->
+                                <div class="d-lg-flex justify-content-between align-items-center mb-4">
+                                    
+                                    <div class="form-check">
+                                        <label for="remember_me" class="form-check-label">
+                                            <x-checkbox id="remember_me" name="remember" />
+                                            <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                                        </label>
+                                    </div>
+                                    @if (Route::has('password.request'))
+                                            <div>
+                                                <a href="{{ route('password.request') }}" wire:navigate>Forgot your password?</a>
+                                            </div>
+                                    @endif
+                                    
+                                </div>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+                                <div>
+                                    <!-- Button -->
+                                    <div class="d-grid">
+                                        <button type="submit" class="btn btn-primary">Sign in</button>
+                                    </div>
+                                </div>
+                                
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-        @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
+        </section>
+        
+    </main>
+    
 </x-guest-layout>
--- resources/views/components/validation-errors.blade.php
+++ resources/views/components/validation-errors.blade.php
@@ -1,11 +1,9 @@
-@if ($errors->any())
-    <div {{ $attributes }}>
-        <div class="font-medium text-red-600">{{ __('Whoops! Something went wrong.') }}</div>
 
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+                            @if(session()->has('error'))
+                                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    </div>
+                            @endif
