b5d04970327be6663aacbe8c32402bff6bf47a6b
MysticMaccc <cosmicsher96@gmail.com>
2023-12-07 13:32:05 +0800

    NA

--- resources/views/livewire/components/show-barcode-component.blade.php
+++ resources/views/livewire/components/show-barcode-component.blade.php
@@ -7,40 +7,45 @@
 
                 <div class="table-responsive mt-2 col-md-12">
                         <x-submit-message />
-                        <table class="table table-striped table-hover">
-                                        <thead>
-                                                <tr>
-                                                        <th>Card Number</th>
-                                                        <th>Barcode</th>
-                                                        <th>Category</th>
-                                                        <th>Category Type</th>
-                                                        <th>Owner</th>
-                                                        <th>Company</th>
-                                                        <th>Valid From</th>
-                                                        <th>Valid To</th>
-                                                        <th>Action</th>
-                                                </tr>
-                                        </thead>
-                                        <tbody>
-                                                @foreach ($barcode_data as $data)
+                        @if (!$barcode_data->isEmpty())
+                                <table class="table table-striped table-hover">
+                                                <thead>
                                                         <tr>
-                                                                <td>{{ $data->card_number }}</td>
-                                                                <td>{{ $data->barcode_value }}</td>
-                                                                <td>{{ $data->category->name  }}</td>
-                                                                <td>{{ $data->category_type->name  }}</td>
-                                                                <td>{{ $data->owner }}</td>
-                                                                <td>{{ $data->company }}</td>
-                                                                <td>{{ $data->start_date }}</td>
-                                                                <td>{{ $data->end_date }}</td>
-                                                                <td>
-                                                                        <button class="btn btn-sm btn-info rounded-3" title="Print Card" wire:click="generate_Barcode({{$data->id}})">
-                                                                                <i class="bi bi-qr-code"></i>
-                                                                        </button>
-                                                                </td>
+                                                                <th>Card Number</th>
+                                                                <th>Barcode</th>
+                                                                <th>Category</th>
+                                                                <th>Category Type</th>
+                                                                <th>Owner</th>
+                                                                <th>Company</th>
+                                                                <th>Valid From</th>
+                                                                <th>Valid To</th>
+                                                                <th>Action</th>
                                                         </tr>
-                                                @endforeach
-                                        </tbody>
-                        </table>
+                                                </thead>
+                                                <tbody>
+                                                        @foreach ($barcode_data as $data)
+                                                                <tr>
+                                                                        <td>{{ $data->card_number }}</td>
+                                                                        <td>{{ $data->barcode_value }}</td>
+                                                                        <td>{{ $data->category->name  }}</td>
+                                                                        <td>{{ $data->category_type->name  }}</td>
+                                                                        <td>{{ $data->owner }}</td>
+                                                                        <td>{{ $data->company }}</td>
+                                                                        <td>{{ $data->start_date }}</td>
+                                                                        <td>{{ $data->end_date }}</td>
+                                                                        <td>
+                                                                                <button class="btn btn-sm btn-info rounded-3" title="Print Card" wire:click="generate_Barcode({{$data->id}})">
+                                                                                        <i class="bi bi-qr-code"></i>
+                                                                                </button>
+                                                                        </td>
+                                                                </tr>
+                                                        @endforeach
+                                                </tbody>
+                                </table>
+                        @else
+                                <h1 class="text-center text-danger mt-5">No data available.</h1>
+                        @endif
+                        
                 </div>
 
                 <div class="col-md-12">
--- resources/views/livewire/components/show-employee-meal-log-component.blade.php
+++ resources/views/livewire/components/show-employee-meal-log-component.blade.php
@@ -7,30 +7,34 @@
         </div>
 
         <div class="table-responsive mt-2 col-md-12">
-                        <table class="table table-striped table-hover">
-                                <thead>
-                                        <tr>
-                                                <th>Card Number</th>
-                                                <th>Name</th>
-                                                <th>Company</th>
-                                                <th>Meal Type</th>
-                                                <th>Date</th>
-                                                <th>Time</th>
-                                        </tr>
-                                </thead>
-                                <tbody>
-                                        @foreach ($employee_meal_log_data as $meal_data)
+                        @if (count($employee_meal_log_data) > 0)
+                                        <table class="table table-striped table-hover">
+                                                <thead>
                                                         <tr>
-                                                        <td>{{ $meal_data->barcode->card_number }}</td>
-                                                        <td>{{ $meal_data->barcode->owner }}</td>
-                                                        <td>{{ $meal_data->barcode->company }}</td>
-                                                        <td>{{ $meal_data->meal_type->name }}</td>
-                                                        <td>{{ $meal_data->date_scanned }}</td>
-                                                        <td>{{ $meal_data->time_scanned }}</td>
+                                                                <th>Card Number</th>
+                                                                <th>Name</th>
+                                                                <th>Company</th>
+                                                                <th>Meal Type</th>
+                                                                <th>Date</th>
+                                                                <th>Time</th>
                                                         </tr>
-                                        @endforeach
-                                </tbody>
-                        </table>
+                                                </thead>
+                                                <tbody>
+                                                        @foreach ($employee_meal_log_data as $meal_data)
+                                                                        <tr>
+                                                                        <td>{{ $meal_data->barcode->card_number }}</td>
+                                                                        <td>{{ $meal_data->barcode->owner }}</td>
+                                                                        <td>{{ $meal_data->barcode->company }}</td>
+                                                                        <td>{{ $meal_data->meal_type->name }}</td>
+                                                                        <td>{{ $meal_data->date_scanned }}</td>
+                                                                        <td>{{ $meal_data->time_scanned }}</td>
+                                                                        </tr>
+                                                        @endforeach
+                                                </tbody>
+                                        </table>
+                        @else
+                                        <h1 class="text-center text-danger mt-5">No data available.</h1>
+                        @endif
         </div>
 
         <div class="col-md-12">
